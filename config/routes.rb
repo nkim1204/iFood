f139e808029511ae2993bed118bec3e9e70bf55e
Andrew Narkewicz <anark@interchange.ubc.ca>
2010-02-02 13:42:53 -0800

    Basic header and layout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
+  map.home '/users/home', :controller => 'users', :action => 'home'
   map.resources :users
 
   map.resource :session
@@ -40,7 +41,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "base"
 
   # See how all your routes lay out with "rake routes"
 
