3c820a823035f36d82bf7dee04ad8626af5a18c8
Leah Russ <bear@bear.local>
2010-03-14 15:29:36 -0700

    search stuff

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,9 @@ class Ingredient < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :recipies, :through => :recipe_ingredients
   has_many :users, :through => :user_ingredients
+
+  #define_index do
+  #  indexes name, :sortable => true  
+  #end
+
 end
