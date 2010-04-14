8efaf3cc10b08d21bd9335a91ada78ec72cad42c
nkim1204 <nkim1204@gmail.com>
2010-04-14 10:35:37 -0700

    Turned delta back on for thinking_sphinx

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -5,6 +5,6 @@ class RecipeIngredient < ActiveRecord::Base
   define_index do
     indexes unit
     has recipe_id, ingredient_id
-    set_property :delta => false
+    set_property :delta => true
   end
 end
